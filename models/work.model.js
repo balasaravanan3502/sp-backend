f96d6354d456491a245efe45e028af84e48d7698
Balasaravanan VP <balasaravananvp02@gmail.com>
2021-07-29 10:12:56 +0530

    Work Schema

--- models/work.model.js
+++ models/work.model.js
@@ -3,12 +3,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  regNo: { type: String, required: true },
-  password: { type: String, required: true },
-  className: { type: String, required: true },
-  classId: { type: String },
+  staffName: { type: String, required: true },
+  completed: { type: Array },
+  unCompleted: { type: Array },
+  lastDate: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Work", workSchema);
